ef6e50f92b32b1098b12b0f17a2ff91d40ab2ddb
Smriti Khanal <smritikhanal45@gmail.com>
2026-01-14 23:10:54 +0545

    added:middleware for auth and error handling

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,10 @@
+// middleware/auth.middleware.js
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  req.user = jwt.verify(token, process.env.JWT_SECRET);
+  next();
+};
